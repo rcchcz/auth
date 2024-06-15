bdeb9d3401a0976f603ec15449f7dc01cffb156a
rcchcz <rcchcz@gmail.com>
2024-06-15 18:18:02 -0300

    feat: login route

--- main.py
+++ main.py
@@ -1,128 +1,31 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
-from passlib.context import CryptContext
+from argon2.exceptions import VerifyMismatchError
+from fastapi import FastAPI, status
+import argon2
 
-SECRET_KEY = '265beb0476e1f9507d7837b3e57da1b4dcb0568cb065f047c7cc41a7245b7d7f'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-db = {
-    'rita': {
-        'username': 'rita',
-        'full_name': 'rita rita',
-        'email': 'rita@mail.com',
-        'hashed_password': '$2b$12$mQiU2U3tHfiwxsk18xfCw.zlQy9HWVrL0KMVqoXLdGPXQvsoCz5tS',
-        'disabled': False
-    }
+mock_db = {
+    'rabbitmq': '$argon2id$v=19$m=65536,t=3,p=4$gmTWdyaVioaDzoXi+Zi7CA$CqPj7mGICVcM4uCcExDH1zacKUMF6ShrirBrt99DH2U',
+    'database': '$argon2id$v=19$m=65536,t=3,p=4$13a6vnlRHiR2jjC6u/Rc6w$LJuTeTRFhplfm87cs8t8k3dt7fRMJpaNllsvFKxNPnE',
+    'dns': '$argon2id$v=19$m=65536,t=3,p=4$EgOOQAdv7q8Rok6muE0Jrw$1P92xuM/XYjdlbN58Fv1lQLLa8FTsvJboMnuitWZHX8'
 }
 
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-
+def get_password_hash_for_user(user_token: str):
+    colon_index = user_token.find(':')
+    return user_token[:colon_index]
 
 app = FastAPI()
+ph = argon2.PasswordHasher()
 
+@app.post('/token/{token}', status_code=204)
+async def login(token: str):
+    user = get_password_hash_for_user(token)
+    hash = mock_db.get(user)
+    print(user)
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def get_user(db, username: str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
-    
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else: 
-        expire = datetime.utcnow() + timedelta(minutes=15)
-
-    to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials', headers={'www-authenticate': 'Bearer'})
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-
-        if username is None:
-            raise credential_exception
-        
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credential_exception
-    
-    user = get_user(db, username=token_data.username)
-    if user is None:
-        raise credential_exception
-    
-    return user
-
-async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail='Inactive user')
-    
-    return current_user
-
-@app.post('/token', response_model=Token)
-async def login_for_acces_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password', headers={'www-authenticate': 'Bearer'})
-
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={'sub': user.username}, expires_delta=access_token_expires)
-
-    return {'access_token': access_token, 'token_type': 'bearer'}
-
-@app.get('/users/me/', response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
-@app.get('/users/me/items')
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{'item_id': 1, 'owner': current_user}]
+    if hash:
+        try:
+            ph.verify(hash, token)
+            return status.HTTP_204_NO_CONTENT
+        except(VerifyMismatchError):
+            return status.HTTP_403_FORBIDDEN
+    else:
+        return status.HTTP_404_NOT_FOUND
